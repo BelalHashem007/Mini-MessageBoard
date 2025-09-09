b3a53ac129c15cfb4ce1af5f2882896d8a8e78ea
BelalHashem007 <belal.hashem.saleh@gmail.com>
2025-09-10 01:07:14 +0300

    Adding validation using express-validator

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,16 +1,38 @@
 const db = require("../db/queries");
 const NotFoundError = require("../errors/NotFoundError");
+const { param, validationResult } = require("express-validator");
+
+const validateId = [
+  param("id")
+    .trim()
+    .notEmpty()
+    .withMessage("Id can`t be empty.")
+    .isInt()
+    .withMessage("Id must be integer."),
+];
 
 async function getMessages(req, res, next) {
   const messages = await db.getMessages();
   if (!messages) next(new Error("Failed to get messages!"));
   res.render("index", { title: "Mini MessageBoard", messages });
 }
-async function getMessageById(req, res, next) {
-  const id = Number(req.params.id);
-  const msg = await db.getMessageById(id);
-  if (!msg) next(new NotFoundError("Message not found."));
-  res.render('message',{msg:msg[0],title:msg[0].user})
-}
+const getMessageById = [
+  validateId,
+  async function (req, res, next) {
+    console.log(req.params)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors.array());
+      const messages = await db.getMessages();
+      return res
+        .status(400)
+        .render("index",{title: "Mini MessageBoard", messages , errors:errors.array()});
+    }
+    const id = Number(req.params.id);
+    const msg = await db.getMessageById(id);
+    if (!msg) next(new NotFoundError("Message not found."));
+    res.render("message", { msg: msg[0], title: msg[0].user });
+  },
+];
 
 module.exports = { getMessages, getMessageById };
--- controller/newController.js
+++ controller/newController.js
@@ -1,16 +1,40 @@
+const { render } = require("ejs");
 const db = require("../db/queries");
+const { body, validationResult } = require("express-validator");
+const validateForm = [
+  body("name")
+    .trim()
+    .notEmpty()
+    .withMessage("Author name can`t be empty.")
+    .isAlpha()
+    .withMessage("Author must contain only alphabetical characters.")
+    .isLength({ max: 20 })
+    .withMessage("Author must be 20 characters at most."),
+  body("message")
+    .trim()
+    .notEmpty()
+    .withMessage("Message can`t be empty.")
+    .isLength({ max: 200 })
+    .withMessage("Message must be 200 characters at most."),
+];
 
 function getForm(req, res) {
   res.render("form");
 }
-async function postMessage(req, res,next) {
-    if (!req.body.name || !req.body.message) next(new Error("Missing data."))
-  const message = {
-    username: req.body.name,
-    message: req.body.message,
-    date: new Date().toUTCString(),
-  };
-  await db.addMessage(message);
-  res.redirect('/');
-}
-module.exports = { getForm,postMessage };
+const postMessage = [
+  validateForm,
+  async function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render('form.ejs',{errors:errors.array()})
+    }
+    const message = {
+      username: req.body.name,
+      message: req.body.message,
+      date: new Date().toUTCString(),
+    };
+    await db.addMessage(message);
+    res.redirect("/");
+  },
+];
+module.exports = { getForm, postMessage };
